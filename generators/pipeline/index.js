09068c6593a901c67e72f893aef6fc03ecd6918f
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-07-30 23:19:24 -0700

    Add linting hooks for pre commit and linted (#21)
    * add eslint/prettier
    
    * integrate husky
    
    * move eslint into its own config file

--- generators/pipeline/index.js
+++ generators/pipeline/index.js
@@ -1,22 +1,22 @@
-"use strict";
-const { spawnSync } = require("child_process");
-const Generator = require("../AbstractGenerator");
+'use strict';
+
+const Generator = require('../AbstractGenerator');
 
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
     // this.option("module", { type: String, required: false, desc: "My option" });
-    this.option("environments", {
+    this.option('environments', {
       type: String,
       required: false,
       // eslint-disable-next-line prettier/prettier
       desc: "Environment names (E.g. build, dev, test, prod) separated by space."
     });
     // the name of the generator
-    this.name = "default";
+    this.name = 'default';
 
     if (opts.module) {
-      console.log("Setting module");
+      console.log('Setting module');
       this.module = opts.module;
     }
   }
@@ -26,12 +26,12 @@ module.exports = class extends Generator {
       if (!this.options.module) {
         await this.prompt([
           {
-            type: "input",
-            name: "module",
+            type: 'input',
+            name: 'module',
             // eslint-disable-next-line prettier/prettier
             message: "What is this module id/key?",
-            default: "hello"
-          }
+            default: 'hello',
+          },
         ]).then(answers => {
           this.options.module = answers.module;
         });
@@ -42,37 +42,37 @@ module.exports = class extends Generator {
 
     await this.prompt([
       {
-        type: "input",
-        name: "name",
+        type: 'input',
+        name: 'name',
         // eslint-disable-next-line prettier/prettier
         message: "What is this module name?",
-        default: this.module.name || this.options.name
+        default: this.module.name || this.options.name,
       },
       {
-        type: "input",
-        name: "version",
+        type: 'input',
+        name: 'version',
         // eslint-disable-next-line prettier/prettier
         message: "What is this module version?",
-        default: this.module.version || this.options.version || "1.0.0"
-      }
+        default: this.module.version || this.options.version || '1.0.0',
+      },
     ]).then(answers => {
       this.module.name = answers.name;
       this.module.version = answers.version;
     });
 
     if (!this.options.environments) {
-      let defaultEnvironments = "build dev test prod";
+      let defaultEnvironments = 'build dev test prod';
       if (this.module.environments) {
-        defaultEnvironments = Object.keys(this.module.environments).join(" ");
+        defaultEnvironments = Object.keys(this.module.environments).join(' ');
       }
       await this.prompt([
         {
-          type: "input",
-          name: "environments",
+          type: 'input',
+          name: 'environments',
           // eslint-disable-next-line prettier/prettier
           message: "What environments are supported by your app? separated by space",
-          default: defaultEnvironments
-        }
+          default: defaultEnvironments,
+        },
       ]).then(answers => {
         this.options.environments = answers.environments;
       });
@@ -80,12 +80,12 @@ module.exports = class extends Generator {
 
     await this.prompt([
       {
-        type: "input",
-        name: "path",
+        type: 'input',
+        name: 'path',
         // eslint-disable-next-line prettier/prettier
         message: "What is the source code directory for this module?",
-        default: this.module.path || "."
-      }
+        default: this.module.path || '.',
+      },
     ]).then(answers => {
       this.module.path = answers.path;
     });
@@ -95,14 +95,14 @@ module.exports = class extends Generator {
     // eslint-disable-next-line prettier/prettier
     this.options.environments.trim().split(" ")
       .forEach(item => {
-        environments[item] = (this.module.environments) ? this.module.environments[item] : {}  
+        environments[item] = this.module.environments ? this.module.environments[item] : {};
       });
     this.module.environments = environments;
 
     const prompts = [];
     Object.keys(environments).forEach(item => {
       prompts.push({
-        type: "input",
+        type: 'input',
         name: `${item.trim()}`,
         message: `What namespace/project name is used for '${item.trim()}?'`,
         // eslint-disable-next-line prettier/prettier
@@ -123,11 +123,11 @@ module.exports = class extends Generator {
     this.fs.copyTpl(
       this.templatePath(`.pipeline`),
       this.destinationPath(`${this.module.path}/.pipeline`),
-      this.module
+      this.module,
     );
     this.fs.copy(
       this.templatePath(`Jenkinsfile`),
-      this.destinationPath(`${this.module.path}/Jenkinsfile`)
+      this.destinationPath(`${this.module.path}/Jenkinsfile`),
     );
   }
 
